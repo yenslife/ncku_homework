850391090b896e5fad03963a900eafc517d63c3e
yenslife <77geo5rge6@gmail.com>
2022-01-10 16:33:22 +0800

    brabrabra

--- Program Design 1/homework/testVS.c
+++ Program Design 1/homework/testVS.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-
-int main(void)
-{
-    printf("this is a test file that haven't been compiled 2");
-    return 0;
+#include <stdlib.h>
+#include <string.h>
+int main() {
+    char s1[] = "";
+    char s2[] = "a";
+    // char *result = calloc(strlen(s1) + strlen(s2) + 1, sizeof(char));
+    char* result = malloc(strlen(s1) + strlen(s2) + 1);
+    // free(s1);
+    free(result);
+    result = NULL;
+    // result = 0;
+    if (result == NULL) {
+        printf("Error: malloc failed in concat\n");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(result, s1);
+    strcat(result, s2);
+    // char *p = concat("abc", "def");
+    printf("%s\n", result);
+    printf("%d\n", *result);
 }
